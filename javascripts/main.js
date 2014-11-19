d699e2dc1ac61d23f7ffc1e2235c4160b2b86855
iDrBacon <iDrBacon@users.noreply.github.com>
2014-11-18 18:17:08 -0600

    1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+var black = [0, 0, 0];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = ("@Rezzux");
+letterColors = [blue]
+if(15 > 16) { 
+    bubbleShape = "tube"
+
+}
+else {
+    bubbleShape = "square"
+}
+drawName(myName, letterColors);
+bounceBubbles()
